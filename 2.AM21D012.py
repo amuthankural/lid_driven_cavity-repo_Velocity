d9d573092aebd7b5079b25c808dcd1f72645aba7
Kuralamuthan <kuralamuthan@live.com>
2021-11-30 08:47:24 +0530

    Logger functionality added

--- 2.AM21D012.py
+++ 2.AM21D012.py
@@ -1,4 +1,5 @@
 import sys
+import logging
 from os import stat, stat_result
 from datetime import datetime, time
 import json
@@ -10,6 +11,11 @@ from numpy.lib.index_tricks import c_
 
 np.set_printoptions(precision=2)
 np.set_printoptions(threshold=sys.maxsize)
+
+
+
+logging.basicConfig(filename='ftcs.log', encoding='utf-8', level=logging.DEBUG,\
+    format = '%(asctime)s:%(name)s:%(message)s')
 
 def prog_run():
     with open('./3.AM21D012_input.json','r') as f:
@@ -27,27 +33,28 @@ def prog_run():
     nu                  = input["nu"]
     dt                  = input["dt"]
     status              = False
-
+    logging.debug('Data imported from JSON file')
 
 
     """-----------Calculated data----------"""
     dx                  = l_i/(n_i-1)                   #Grid length along 'x' direction
     dy                  = b_j/(m_j-1)                   #Grid length along 'y' direction
     beta                = dx/dy
-    dim                     = (n_i,m_j)
+    dim                 = (n_i,m_j)
     
     """---------------Domains--------------"""
-    om_old                  = discretize(n_i,m_j)
+    n_om_domain             = discretize(n_i,m_j)
     om_domain               = discretize(n_i,m_j)
     si_domain               = discretize(n_i,m_j)
     u_domain                = discretize(n_i,m_j)
     v_domain                = discretize(n_i,m_j)
-    #vel_domain              = np.zeros([n_i, m_j, 2], dtype= float)
-    #print(vel_domain.shape)
-    print((dx,dy))
+    logging.debug('Domain arrays created')
+    
 
     iteration = 1
-    while (status == False):
+    logging.debug('Stepping into iterations')
+    while (iteration <= 20):
+        logging.debug('Iteration step: {}'.format(iteration))
         u_domain = vel_bound_condition(u_domain,bc_u,dim)
         v_domain = vel_bound_condition(v_domain,bc_v,dim)
         om_domain = om_calc(om_domain,u_domain,v_domain,dx,dy,dim)
@@ -59,6 +66,8 @@ def prog_run():
         v_domain = v_calc(si_domain,v_domain,dx,dim)
         status = converge(om_domain,n_om_domain,criteria)
         iteration += 1
+    logging.debug('Iterations completed. Status: {}\n'.format(status))
+
 
 
 def discretize(n_i,m_j):
@@ -77,11 +86,10 @@ def vel_bound_condition(domain,bc,dim):
 
 #Central difference scheme
 def om_calc(omega,u,v,dx,dy,dim):
-    print("dimension: ",dim,"\n")
     for i in range(1,dim[0]-1):
         for j in range(1,dim[1]-1):
             omega[i][j] = ((v[i][j+1] - 2*v[i][j] + v[i][j-1])/(dx*dx)) - ((u[i+1][j] - 2*u[i][j] + u[i-1][j])/(dy*dy))
-            #print("iteration: ",i,j,"\n")
+
     return omega
 
 
@@ -89,11 +97,11 @@ def om_calc(omega,u,v,dx,dy,dim):
 def vorticity_bound(omega,psi,u,v,dx,dy,dim):
     m = dim[1]-1
     n = dim[0]-1
-    for i in range(n+1):
+    for i in range(1,n+1):
         omega[i][0] = 2*(psi[i][0]-psi[i][1])/(dx*dx) - 2*(v[i][0])/(dx)
         omega[i][m] = 2*(psi[i][m]-psi[i][m-1])/(dx*dx) + 2*(v[i][m])/(dx)
 
-    for j in range(m+1):
+    for j in range(1,m+1):
         omega[0][j] = 2*(psi[0][j]-psi[1][j])/(dy*dy) + 2*(u[0][j])/(dy)
         omega[n][j] = 2*(psi[n][j]-psi[n-1][j])/(dy*dy) - 2*(u[n][j])/(dy)
 
@@ -101,8 +109,8 @@ def vorticity_bound(omega,psi,u,v,dx,dy,dim):
 
 def vorticity(omega,u,v,dx,dy,dt,nu,dim):
     n_domain = discretize(dim[0],dim[1])
-    for i in range(dim[1]):
-        for j in range(dim[0]):
+    for i in range(1,dim[1]-1):
+        for j in range(1,dim[0]-1):
             n_domain[i][j] = omega[i][j] - (u[i][j]*(omega[i+1][j]-omega[i-1][j])/(2*dx))\
                 - (v[i][j]*(omega[i][j+1]-omega[i][j-1])/(2*dy))\
                     + ((nu*dt)*(omega[i+1][j] - 2*omega[i][j] + omega[i-1][j])/(dx*dx))\
@@ -142,4 +150,5 @@ def converge(o_omega,n_omega,criteria):
     else:
         return False
 
-prog_run()
+if (__name__ == "__main__"):
+    prog_run()
